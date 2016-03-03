e8b7d7e84182cd1575b490ea4a110efc46f78bc9
Vishal Patel <vishal.patel@traveledge.com>
2016-03-03 18:26:52 -0500

    Added comments

--- C#/DetrackSample/Program.cs
+++ C#/DetrackSample/Program.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System.IO;
 
 namespace DetrackSample
 {
@@ -23,6 +25,7 @@ namespace DetrackSample
     #region MainProgram
     public static void Main(string[] args)
     {
+      /*Program entry point*/
       MakeAPICall().Wait();
     }
     #endregion
@@ -37,20 +40,20 @@ namespace DetrackSample
       //1. Add Deliveries
       await AddDeliveries();
 
-      //2. Edit Deliveries
-      await EditDeliveries();
+      //2.Edit Deliveries
+     await EditDeliveries();
 
-      //4. View All Deliveries
+      //4.View All Deliveries
       await ViewAllDeliveries();
 
-      //5. download delivery pod signature image file
-      await DownloadDeliveryPodSignature();
+      //5.download delivery pod signature image file
+     await DownloadDeliveryPodSignature();
 
-      //6. View All Deliveries
+      //6.View All Deliveries
       await ViewAllVehicles();
 
-      //3. Delete Deliveries
-      await DeleteDeliveries();
+      //3.Delete Deliveries
+     await DeleteDeliveries();
 
     }
     #endregion
@@ -64,13 +67,12 @@ namespace DetrackSample
     {
       var client = new HttpClient
       {
-        BaseAddress = new Uri("https://app.detrack.com/api/v1/")
-
+        BaseAddress = new Uri("https://app.detrack.com/api/v1/") //Set the base address of the detrack api.
       };
 
       client.DefaultRequestHeaders.Accept.Clear();
       client.DefaultRequestHeaders.Add("X-API-KEY", ApiKey); //Set Custom Header for defining API key.
-      client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+      client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json")); //Set the accept header
       return client;
     }
 
@@ -80,15 +82,20 @@ namespace DetrackSample
     /// <returns></returns>
     private static async Task AddDeliveries()
     {
+      //Initiate Http Client
       using (var client = GetHttpClient())
       {
+        //Get sample item that will be sent to Add Delivery API Call
         var obj = GetSampleDeliveryItem();
         try
         {
           var response = await client.PostAsJsonWithNoCharSetAsync("deliveries/create.json", obj);
           if (response.IsSuccessStatusCode)
           {
-            Console.WriteLine("Response: {0}", response.Content.ReadAsStringAsync().Result);
+            var result = response.Content.ReadAsStringAsync().Result;
+            Console.WriteLine("Response: {0}", result);
+            var resultObj = JObject.Parse(result);
+            Console.WriteLine(resultObj["results"][0]["do"]);//Access the property of JSON object returened in response
           }
         }
         catch (Exception ex)
@@ -104,16 +111,23 @@ namespace DetrackSample
     /// <returns></returns>
     private static async Task EditDeliveries()
     {
+      //Initiate the HTTP Cient
       using (var client = GetHttpClient())
       {
+        //Get the delivery items that needs to be updated
         var obj = GetSampleDeliveryItem();
-        obj[0].delivery_time = "12:00 PM - 03:00 PM";
+        obj[0].delivery_time = "12:00 PM - 03:00 PM"; //Update one of the delivery time
         try
         {
+          //Send POST request
           var response = await client.PostAsJsonWithNoCharSetAsync("deliveries/update.json", obj);
           if (response.IsSuccessStatusCode)
           {
-            Console.WriteLine("Response: {0}", response.Content.ReadAsStringAsync().Result);
+            //Read the result property of the response
+            var result = response.Content.ReadAsStringAsync().Result;
+            Console.WriteLine("Response: {0}", result);
+            var resultObj = JObject.Parse(result);
+            Console.WriteLine(resultObj["results"][0]["do"]);//Access the property of JSON object returened in response
           }
         }
         catch (Exception ex)
@@ -129,15 +143,19 @@ namespace DetrackSample
     /// <returns></returns>
     private static async Task DeleteDeliveries()
     {
+      //Initiate the HTTP Client to send request
       using (var client = GetHttpClient())
       {
+        //Get the item that is to be deleted
         var obj = GetSampleDeleteDeliveryItem();
         try
         {
+          //Initiate POST request
           var response = await client.PostAsJsonWithNoCharSetAsync("deliveries/delete.json", obj);
           if (response.IsSuccessStatusCode)
           {
-            Console.WriteLine("Response: {0}", response.Content.ReadAsStringAsync().Result);
+            var result = response.Content.ReadAsStringAsync().Result;
+            Console.WriteLine("Response: {0}", result);
           }
         }
         catch (Exception ex)
@@ -153,15 +171,20 @@ namespace DetrackSample
     /// <returns></returns>
     private static async Task ViewAllDeliveries()
     {
+      //initiate HTTP client
       using (var client = GetHttpClient())
       {
+        //Initiate the POST data - Date for which all deliveries will be fetched data
         var obj = new ViewAllDeliveries { date = "2014-02-13" };
         try
         {
+          //Initiate the POST request
           var response = await client.PostAsJsonWithNoCharSetAsync("deliveries/view/all.json", obj);
           if (response.IsSuccessStatusCode)
           {
-            Console.WriteLine("Response: {0}", response.Content.ReadAsStringAsync().Result);
+            //Read response result property which contains returned JSON object
+            var result = response.Content.ReadAsStringAsync().Result;
+            Console.WriteLine("Response: {0}", result);
           }
         }
         catch (Exception ex)
@@ -178,15 +201,26 @@ namespace DetrackSample
     /// <returns></returns>
     private static async Task DownloadDeliveryPodSignature()
     {
+      //Initiate HTTP client
       using (var client = GetHttpClient())
       {
-        var obj = new DownloadDeliveryPodSignatureSearchCriteria { date = "2014-02-13", Do = "DO140213001" };
+        //Initialiaze POST data, Date and DO for which signature file will be downloaded
+        var obj = new DownloadDeliveryPodSignatureSearchCriteria { date = "2016-03-02", Do = "DO140211001" };
         try
         {
           var response = await client.PostAsJsonWithNoCharSetAsync("deliveries/signature.json", obj);
           if (response.IsSuccessStatusCode)
           {
-            Console.WriteLine("Response: {0}", response.Content.ReadAsStringAsync().Result);
+            var httpStream = await response.Content.ReadAsStreamAsync();
+            //Save to local file system
+            var filePath = Path.Combine(".", string.Format("{0}{1}{2}{3}.jpeg",
+                       DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second, "signature"));
+            using (var fileStream = File.Create(filePath))
+            using (var reader = new StreamReader(httpStream))
+            {
+              httpStream.CopyTo(fileStream);
+              fileStream.Flush();
+            }
           }
           else
           {
@@ -206,13 +240,16 @@ namespace DetrackSample
     /// <returns></returns>
     private static async Task ViewAllVehicles()
     {
+      //Initiate HTTP Client
       using (var client = GetHttpClient())
       {
         try
         {
+          //POST with empty body
           var response = await client.PostAsJsonWithNoCharSetAsync("vehicles/view/all.json", new object());
           if (response.IsSuccessStatusCode)
           {
+            //Read the response as JSON object containing all vehicle information
             Console.WriteLine("Response: {0}", response.Content.ReadAsStringAsync().Result);
           }
         }
